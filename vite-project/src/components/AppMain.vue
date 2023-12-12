0849e74b9cedf9a1ff3b2de63300cb43fc693193
DavideCiammetti <davideciammetti97@gmail.com>
2023-12-12 15:46:08 +0100

    feat: installazione axios, aggiunta api e chiamata con axios in main

--- vite-project/src/components/AppMain.vue
+++ vite-project/src/components/AppMain.vue
@@ -1,11 +1,25 @@
 <script>
+import axios from 'axios';
+
      export default{
         name: 'header',
-    }
+            data(){
+            return{
+                'cards': [],
+                'apiUrl': 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+            };
+        },
+        created(){
+            axios.get(this.apiUrl).then((response)=>{
+                this.cards = response.data.results;
+            });
+        },
+    };
+   
 </script>
 
 <template>
-    main
+    
 </template>
 
 <style scoped lang="scss">
